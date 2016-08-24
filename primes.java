47ecfe46f0c56f44dd716c4e20a6f97c405ef789
vpiotr <vpiotr@poczta.onet.pl>
2016-08-24 22:53:46 +0200

    optimization - removed autoboxing, array instead of ArrayList

--- primes.java
+++ primes.java
@@ -2,51 +2,69 @@ import java.util.*;
 import java.lang.Math;
 
 class PrimeNumbersGenerator {
-	ArrayList<Integer> get_primes7(int n) {
-		ArrayList<Integer> res = new ArrayList<Integer>();
+    int[] get_primes7(int n) {
+        int[] res;
 
-		if (n < 2) return res;
-		if (n == 2) {
-			res.add(2);
-			return res;
-		}
-		ArrayList<Integer> s = new ArrayList<Integer>();
-		for (int i = 3; i < n + 1; i += 2) {
-			s.add(i);
-		}
-		int mroot = (int)Math.sqrt(n);
-		int half = s.size();
-		int i = 0;
-		int m = 3;
-		while (m <= mroot) {
-			if (s.get(i) != 0) {
-				int j = (int)((m*m - 3)/2);
-				s.set(j, 0);
-				while (j < half) {
-					s.set(j, 0);
-					j += m;
-				}
-			}
-			i = i + 1;
-			m = 2*i + 3;
-		}
-		res.add(2);
-		for (int it = 0; it < s.size(); ++it) {
-			if (s.get(it) != 0) {
-				res.add(s.get(it));
-			}
-		}
+        if (n < 2)
+            return new int[0];
+        if (n == 2) {
+            res = new int[1];
+            res[0] = 2;
+            return res;
+        }
 
-		return res;
-	}
+        int[] s = new int[(n + 1 - 3) / 2];
+        int prime_idx = 0;
+        for (int i = 3; i < n + 1; i += 2) {
+            s[prime_idx] = i;
+            prime_idx++;
+        }
+
+        int mroot = (int) Math.sqrt(n);
+        int half = s.length;
+        int i = 0;
+        int m = 3;
+        while (m <= mroot) {
+            if (s[i] != 0) {
+                int j = (int) ((m * m - 3) / 2);
+                s[j] = 0;
+                while (j < half) {
+                    s[j] = 0;
+                    j += m;
+                }
+            }
+            i = i + 1;
+            m = 2 * i + 3;
+        }
+
+        int res_size = 1;
+        for (int it = 0; it < s.length; ++it) {
+            if (s[it] != 0) {
+                res_size++;
+            }
+        }
+
+        res = new int[res_size];
+        res[0] = 2;
+        prime_idx = 1;
+
+        for (int it = 0; it < s.length; ++it) {
+            if (s[it] != 0) {
+                res[prime_idx] = s[it];
+                prime_idx++;
+            }
+        }
+
+        return res;
+    }
 }
 
 class PrimeNumbersBenchmarkApp {
 	public static void main(String[] args) {
-		ArrayList<Integer> res;
+        int[] res;
 		for (int i = 1; i <= 10; ++i) {
-			res = (new PrimeNumbersGenerator()).get_primes7(10000000);
-			System.out.format("Found %d prime numbers.\n", res.size());
+            res = (new PrimeNumbersGenerator2()).get_primes7(10000000);
+            System.out.format("Found %d prime numbers.\n", res.length);
 		}
 	}
 }
