013620adf2c2d89f17a2d73241f034b1f317a625
mayr-work <crtomir.majer1@um.si>
2016-09-21 19:13:25 +0200

    java performance improvement

--- primes.java
+++ primes.java
@@ -2,55 +2,55 @@ import java.util.*;
 import java.lang.Math;
 
 class PrimeNumbersGenerator {
-	ArrayList<Integer> get_primes7(int n) {
-		ArrayList<Integer> res = new ArrayList<Integer>();
+    List<Integer> get_primes7(int n) {
+        List<Integer> res = new LinkedList<Integer>();
 
-		if (n < 2) return res;
-		if (n == 2) {
-			res.add(2);
-			return res;
-		}
-		ArrayList<Integer> s = new ArrayList<Integer>();
-		for (int i = 3; i < n + 1; i += 2) {
-			s.add(i);
-		}
-		int mroot = (int)Math.sqrt(n);
-		int half = s.size();
-		int i = 0;
-		int m = 3;
-		while (m <= mroot) {
-			if (s.get(i) != 0) {
-				int j = (int)((m*m - 3)/2);
-				s.set(j, 0);
-				while (j < half) {
-					s.set(j, 0);
-					j += m;
-				}
-			}
-			i = i + 1;
-			m = 2*i + 3;
-		}
-		res.add(2);
-		for (int it = 0; it < s.size(); ++it) {
-			if (s.get(it) != 0) {
-				res.add(s.get(it));
-			}
-		}
+        if (n < 2) return res;
+        if (n == 2) {
+            res.add(2);
+            return res;
+        }
+        List<Integer> s = new ArrayList<Integer>();
+        for (int i = 3; i < n + 1; i += 2) {
+            s.add(i);
+        }
+        int mroot = (int) Math.sqrt(n);
+        int half = s.size();
+        int i = 0;
+        int m = 3;
+        while (m <= mroot) {
+            if (s.get(i) != 0) {
+                int j = (int) ((m * m - 3) / 2);
+                s.set(j, 0);
+                while (j < half) {
+                    s.set(j, 0);
+                    j += m;
+                }
+            }
+            i = i + 1;
+            m = 2 * i + 3;
+        }
+        res.add(2);
+        for (int it = 0; it < s.size(); ++it) {
+            if (s.get(it) != 0) {
+                res.add(s.get(it));
+            }
+        }
 
-		return res;
-	}
+        return res;
+    }
 }
 
 class PrimeNumbersBenchmarkApp {
-	public static void main(String[] args) {
-		long startTime = System.currentTimeMillis();
-		long periodTime = Long.parseLong(System.getenv("RUN_TIME"), 10) * 1000;
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        long periodTime = Long.parseLong(System.getenv("RUN_TIME"), 10) * 1000;
 
-		ArrayList<Integer> res;
+        List<Integer> res;
 
-		while ((System.currentTimeMillis() - startTime) < periodTime) {
-			res = (new PrimeNumbersGenerator()).get_primes7(10000000);
-			System.out.format("Found %d prime numbers.\n", res.size());
-		}
-	}
+        while ((System.currentTimeMillis() - startTime) < periodTime) {
+            res = (new PrimeNumbersGenerator()).get_primes7(10000000);
+            System.out.format("Found %d prime numbers.\n", res.size());
+        }
+    }
 }
